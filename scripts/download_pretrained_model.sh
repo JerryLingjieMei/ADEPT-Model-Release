52da9bf01c6aa898ed4a410e6db6d33a5906285b
JerryLingjieMei <jerrymei@mit.edu>
2019-10-23 19:19:35 -0400

    Fix naming error in download pretrained model

--- scripts/download_pretrained_model.sh
+++ scripts/download_pretrained_model.sh
@@ -8,12 +8,12 @@ echo "Downloading pre-trained weights for detection"
 URL=http://physadept.csail.mit.edu/assets/ADEPT-detection-0050000.tar.gz
 echo ${URL}
 curl ${URL} | tar -zx -C output/default_detection
-echo 'output/default-detection/ADEPT-detection-0050000.pth' > output/default_detection/last_checkpoint.txt
+echo 'output/default_detection/ADEPT-detection-0050000.pth' > output/default_detection/last_checkpoint
 
 echo "Downloading pre-trained weights for derender"
 
 URL=http://physadept.csail.mit.edu//assets/ADEPT-derender-0050000.tar.gz
 echo ${URL}
 curl ${URL} | tar -zx -C output/default_derender
-echo 'output/default-derender/ADEPT-detection-0050000.pth' > output/default_derender/last_checkpoint.txt
+echo 'output/default_derender/ADEPT-derender-0050000.pth' > output/default_derender/last_checkpoint
 
